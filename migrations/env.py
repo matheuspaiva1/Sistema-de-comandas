e6c7df1f631ce8087166b337658a5ee1142b971f
Miqueias Bento <miqueiasbto32@alu.ufc.br>
2026-05-16 21:13:02 -0300

    chore: adicionando conexão com banco de dados postgreSQL

--- migrations/env.py
+++ migrations/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from sqlmodel import SQLModel
+from app.core.config import settings
 
 # Importa todos os models para registrá-los no SQLModel.metadata
 import app.models  # noqa: F401
@@ -14,6 +15,10 @@ import app.models  # noqa: F401
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.database_url
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
